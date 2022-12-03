9b59f1aeb01f49f1b82c8cf3f9798ecac8c47e1a
rationew <julieynjoroge@gmail.com>
2022-12-03 02:36:10 -0800

    alphabet

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from print_alphabet import print_alpha
+print_alpha()
---
+++ 0x02-python-import_modules/print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_alpha():
+    for n in range(65, 91):
+        print("{:c}".format(n), end="")
+    print()

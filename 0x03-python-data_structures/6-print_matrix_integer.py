7c5d8cb9f6811549b04926306de1427dcf3c80df
rationew <julieynjoroge@gmail.com>
2022-12-05 09:04:47 -0800

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,4 +9,3 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(j), end=" ")
             l = l + 1
         print()
-
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -9,3 +9,29 @@ def print_list_integer(my_list=[]):
 								def max_integer(my_list=[]):
 									def divisible_by_2(my_list=[]):
 										def delete_at(my_list=[], idx=0):
+											int is_palindrome(listint_t **head);
+											#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for project
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+int is_palindrome(listint_t **head);
+
+#endif /* LISTS_H */

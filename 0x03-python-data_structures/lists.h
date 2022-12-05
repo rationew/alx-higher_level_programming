9dd75dec7b5f03116b9c2851f12fcdb06c5ba4ee
rationew <julieynjoroge@gmail.com>
2022-12-05 06:36:24 -0800

    selete at

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -8,3 +8,4 @@ def print_list_integer(my_list=[]):
 							def multiple_returns(sentence):
 								def max_integer(my_list=[]):
 									def divisible_by_2(my_list=[]):
+										def delete_at(my_list=[], idx=0):

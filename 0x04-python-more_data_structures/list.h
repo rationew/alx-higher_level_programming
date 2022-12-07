6fa07478596b6196a58544c0db308fb912520818
rationew <julieynjoroge@gmail.com>
2022-12-07 03:41:18 -0800

    multiply

--- 0x04-python-more_data_structures/list.h
+++ 0x04-python-more_data_structures/list.h
@@ -7,3 +7,4 @@ def square_matrix_simple(matrix=[]):
 						def print_sorted_dictionary(a_dictionary):
 							def update_dictionary(a_dictionary, key, value):
 								def simple_delete(a_dictionary, key=""):
+								def multiply_by_2(a_dictionary):

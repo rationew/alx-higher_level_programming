601b8b3e3f57078b503fc1ffc9e6487be8ef99d1
rationew <julieynjoroge@gmail.com>
2022-12-07 03:21:30 -0800

    keys

--- 0x04-python-more_data_structures/list.h
+++ 0x04-python-more_data_structures/list.h
@@ -3,3 +3,4 @@ def square_matrix_simple(matrix=[]):
 		def uniq_add(my_list=[]):
 			def common_elements(set_1, set_2):
 				def only_diff_elements(set_1, set_2):
+					def number_keys(a_dictionary):

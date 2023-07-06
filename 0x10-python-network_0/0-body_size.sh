980b58f0af4b341dadc4307e282a46cf444a33c7
rationew <you@example.com>
2023-07-05 17:10:19 -0700

    sends a GET request

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
 
-#displays the size of the body of the response
-
-curl -s -o /dev/null -w "%{size_download}" "$1"
